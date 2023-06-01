8b5c2d03329952135762f649d758818d2ebc8afc
webtech <purposeresearch923@gmail.com>
2023-06-01 11:44:50 +0530

    law

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './Style/Style.css'
 import './Style/Responsive.css'
 import Header from './Header/Header';
 import Home from './Components/Home';
 import Footer from './Footer/Footer'
 import PracticeAreaFull from './Components/PracticeAreaFull';
- 
+
 
 
 function App() {
   return (
-  <>
-  <BrowserRouter>
-  <Header/>
-  <Routes>
-  <Route index element={<Home/>}/>
-  <Route path='/our-practice-area' element={<PracticeAreaFull/>}/>
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
-  </>
+    <>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/our-practice-area' element={<PracticeAreaFull />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
+    </>
   );
 }
 
